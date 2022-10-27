4ab7826147501f059e249e36e85673161c862d6a
lamcc0603 <chungchilam12@gmail.com>
2022-10-27 19:42:52 +0700

    sửa lại scss

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -4,106 +4,107 @@ import logo from '../../assets/images/4TL_Logo_png.png';
 
 const Footer = () => {
     return (
-        <footer className='footer'>
-            <div className='footer__logo'>
-                <Link to='/'>
-                    <img src={logo} alt='logo' width={300} />
+        <footer className="footer">
+            <div className="footer__logo">
+                <Link to="/">
+                    <img src={logo} alt="logo" width={300} />
                 </Link>
             </div>
 
-            <div class="footer__contact">
+            <div className="footer__contact">
                 <h3>Contact</h3>
-                <ul class="menu">
-                    <li class="menu__item">
-                        <p class="menu__link">
-                        <i class="fa-solid fa-house"></i> 778/B1 Nguyễn Kiệm, P.4, Q. Phú Nhuận, TP. Hồ Chí Minh.
+                <ul className="menu">
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-house"></i> 778/B1 Nguyễn Kiệm, P.4, Q. Phú Nhuận, TP. Hồ Chí
+                            Minh.
                         </p>
                     </li>
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-phone"></i> 02873088800
-                    </p>
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-phone"></i> 02873088800
+                        </p>
                     </li>
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-envelope"></i> dvsvpoly.hcm@poly.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i>
-                        Đào tạo:daotaofpoly.hcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> khaothi.fplhcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> taichinhfplhcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> qhdn.fplhcm@fe.edu.vn
-                    </p>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="footer__contact">
-                <h3>Contact</h3>
-                <ul class="menu">
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-house"></i> 778/B1 Nguyễn Kiệm, P.4, Q. Phú
-                        Nhuận, TP. Hồ Chí Minh.
-                    </p>
-                    </li>
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-phone"></i> 02873088800
-                    </p>
-                    </li>
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-envelope"></i> dvsvpoly.hcm@poly.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i>
-                        Đào tạo:daotaofpoly.hcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> khaothi.fplhcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> taichinhfplhcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> qhdn.fplhcm@fe.edu.vn
-                    </p>
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-envelope"></i> dvsvpoly.hcm@poly.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i>
+                            Đào tạo:daotaofpoly.hcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> khaothi.fplhcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> taichinhfplhcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> qhdn.fplhcm@fe.edu.vn
+                        </p>
                     </li>
                 </ul>
             </div>
 
-            <div class="footer__contact">
+            <div className="footer__contact">
                 <h3>Contact</h3>
-                <ul class="menu">
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-house"></i> 778/B1 Nguyễn Kiệm, P.4, Q. Phú
-                        Nhuận, TP. Hồ Chí Minh.
-                    </p>
+                <ul className="menu">
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-house"></i> 778/B1 Nguyễn Kiệm, P.4, Q. Phú Nhuận, TP. Hồ Chí
+                            Minh.
+                        </p>
                     </li>
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-phone"></i> 02873088800
-                    </p>
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-phone"></i> 02873088800
+                        </p>
                     </li>
-                    <li class="menu__item">
-                    <p class="menu__link">
-                        <i class="fa-solid fa-envelope"></i> dvsvpoly.hcm@poly.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i>
-                        Đào tạo:daotaofpoly.hcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> khaothi.fplhcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> taichinhfplhcm@fe.edu.vn
-                        <br />
-                        <i class="fa-solid fa-envelope"></i> qhdn.fplhcm@fe.edu.vn
-                    </p>
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-envelope"></i> dvsvpoly.hcm@poly.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i>
+                            Đào tạo:daotaofpoly.hcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> khaothi.fplhcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> taichinhfplhcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> qhdn.fplhcm@fe.edu.vn
+                        </p>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="footer__contact">
+                <h3>Contact</h3>
+                <ul className="menu">
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-house"></i> 778/B1 Nguyễn Kiệm, P.4, Q. Phú Nhuận, TP. Hồ Chí
+                            Minh.
+                        </p>
+                    </li>
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-phone"></i> 02873088800
+                        </p>
+                    </li>
+                    <li className="menu__item">
+                        <p className="menu__link">
+                            <i className="fa-solid fa-envelope"></i> dvsvpoly.hcm@poly.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i>
+                            Đào tạo:daotaofpoly.hcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> khaothi.fplhcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> taichinhfplhcm@fe.edu.vn
+                            <br />
+                            <i className="fa-solid fa-envelope"></i> qhdn.fplhcm@fe.edu.vn
+                        </p>
                     </li>
                 </ul>
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import App from './App';
 import './sass/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
